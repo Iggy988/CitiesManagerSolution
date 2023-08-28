4b170d3e59a9ee8932edadcf074e9b0039737b7a
Iggy988 <igor.rodic1988@gmail.com>
2023-08-28 23:10:09 +0200

    Adding Swagger OpenAPI

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -11,11 +11,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+//Swagger
+builder.Services.AddEndpointsApiExplorer();//Generates desceiption for all endpoints
+builder.Services.AddSwaggerGen();// generates OpenAPI specificasion
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHsts();
 app.UseHttpsRedirection();
+
+app.UseSwagger(); // creates endpoint for swagger.json
+app.UseSwaggerUI(); //creates swaggger UI for testing all Web API endpoints/action methods
 
 app.UseAuthorization();
 
