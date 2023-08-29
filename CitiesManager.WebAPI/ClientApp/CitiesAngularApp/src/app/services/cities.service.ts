f469229b4e87efc9a8d685cac81be772c4c2a24f
Iggy988 <igor.rodic1988@gmail.com>
2023-08-29 22:05:39 +0200

    Request to WebAPI trought Angular App

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { City } from "../models/city";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitiesService {
   cities: City[] = [];
-  constructor() {
-    this.cities = [
-      new City("101", "New York"),
-      new City("102", "London"),
-      new City("103", "Berlin"),
-      new City("104", "NParis"),
-    ]
+  constructor(private httpClient: HttpClient) {
+    //this.cities = [
+    //  new City("101", "New York"),
+    //  new City("102", "London"),
+    //  new City("103", "Berlin"),
+    //  new City("104", "Paris"),
+    //]
+  }
+
+  public getCities(): Observable<City[]> {
+    //return this.cities;
+    return this.httpClient.get<City[]>("https://localhost:7187/api/v1/cities")
   }
 }
