c966e133594b640818cce558cab5f8ca6f06c9d9
Iggy988 <igor.rodic1988@gmail.com>
2023-08-30 22:50:48 +0200

    Adding PUT Angular method

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
@@ -10,30 +10,28 @@ const API_BASE_URL: string = "https://localhost:7187/api/";
 })
 export class CitiesService {
   cities: City[] = [];
+
   constructor(private httpClient: HttpClient) {
-    //this.cities = [
-    //  new City("101", "New York"),
-    //  new City("102", "London"),
-    //  new City("103", "Berlin"),
-    //  new City("104", "Paris"),
-    //]
   }
 
   public getCities(): Observable<City[]> {
-    //return this.cities;
-
     let headers = new HttpHeaders();
     headers = headers.append("Authorization", "Bearer mytoken");
 
-    return this.httpClient.get<City[]>(`${API_BASE_URL}v1/cities`, { headers: headers });
+    return this.httpClient.get<City[]>(`${API_BASE_URL}v1/cities`, { headers: headers })
   }
 
-  public postCities(city: City): Observable<City> {
-    //return this.cities;
-
+  public postCity(city: City): Observable<City> {
     let headers = new HttpHeaders();
     headers = headers.append("Authorization", "Bearer mytoken");
 
-    return this.httpClient.post<City>(`${API_BASE_URL}v1/cities`, city, { headers: headers });
+    return this.httpClient.post<City>(`${API_BASE_URL}v1/cities`, city, { headers: headers })
+  }
+
+  public putCity(city: City): Observable<string> {
+    let headers = new HttpHeaders();
+    headers = headers.append("Authorization", "Bearer mytoken");
+
+    return this.httpClient.put<string>(`${API_BASE_URL}v1/cities/${city.cityID}`, city, { headers: headers })
   }
 }
